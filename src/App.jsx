08293115ff8b584915d12d93057bb4374f2ccc3a
Марсель <kabaler2006@gmail.com>
2025-09-11 01:47:39 +0300

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Banner from "./components/MainPage/Banner/Banner";
 export default function App() {
   return (
     <>
       <Header/>
+      <Banner/> 
       <Footer/>
     </>
   );
---
+++ src/components/MainPage/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import './Banner.css'
+export default function Banner(){
+  return(
+    <>
+      <div className="banner">
+        <div className="container">
+          <div className="banner-inner">
+            <div className="bannner-card">
+              <h1 className="banner-title">Вазелиновое масло для промышленных предприятий</h1>
+              <p className="banner-undertitle">
+Высокочистое, очищенное, соответствует всем нормам и стандартам качества.
+              </p>
+              <button className="banner-button">Для промышленности</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
