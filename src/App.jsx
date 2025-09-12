6d87dde963aba3cb7298e4106f9bba52ed7e3110
Марсель <kabaler2006@gmail.com>
2025-09-12 12:22:19 +0300

    block steps of order

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Banner from "./components/MainPage/Banner/Banner";
+import StepsOfOrder from "./components/MainPage/StepsOfOrder/StepsOfOrder";
 export default function App() {
   return (
     <>
       <Header/>
       <Banner/> 
+      <StepsOfOrder/>
       <Footer/>
     </>
   );
---
+++ src/components/MainPage/StepsOfOrder/StepsOfOrder.jsx
@@ -0,0 +1,29 @@
+import "./StepsOfOrder.css"
+export default function StepsOfOrder(){
+  const stepsoforder = [
+    {id: 1, title: 'Оставляете заявку', undertitle: 'Отправляете запрос на сайте. Укажите интересующий объём, назначение и требования к маслу.'},
+    {id: 2, title: 'Получаете коммерческое предложение', undertitle: 'Менеджер связывается с вами, уточняет детали и высылает прайс, сертификаты и условия поставки.'},
+    {id: 3, title: 'Согласовываем поставку', undertitle: 'После согласования резервируем товар подготавливаем полный пакет документов — УПД, паспорт качества.'},
+    {id: 4, title: 'Организуем доставку', undertitle: 'Предоставляем полный пакет сопроводительных документов: УПД и паспорт качества.'}
+  ];
+  return(
+    <>
+      <section className="steps-of-order">
+        <div className="container">
+          <div className="steps-of-order-inner">
+            <h1 className="steps-of-partnership">Этапы сотрудничества</h1>
+            {stepsoforder.map((step) => 
+              <div key={step.id} className="step-item">
+                <div className="step-item-number">{step.id}</div>
+                <div className="step-item-text">
+                  <div className="step-item-title">{step.title}</div>
+                  <div className="step-item-undertitle">{step.undertitle}</div>
+                </div>
+              </div>
+            )}
+          </div>  
+        </div>
+      </section>
+    </>
+  );
+}
